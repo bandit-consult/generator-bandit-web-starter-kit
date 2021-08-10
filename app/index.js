c94c78098fc79c92ea1664155c5816014079d5a9
Kofi Ramos Amoussou <karsoft92@gmail.com>
2021-08-10 20:39:06 +0000

    v1

--- app/index.js
+++ app/index.js
@@ -7,6 +7,45 @@ module.exports = class extends Generator{
 		this.log('Welcom to bandit web starter kit !!')
 	}
 
+	// copying files
+	
+	writing(){
+
+		this.fs.copy(
+			this.templatePath('index.html'),
+			this.destinationPath('index.html'),
+		);
+
+		this.fs.copy(
+			this.templatePath('css/style.css'),
+			this.destinationPath('css/style.css'),
+		);
+
+		this.fs.copy(
+			this.templatePath('js/app.js'),
+			this.destinationPath('js/app.js'),
+		);
+
+		this.fs.copy(
+			this.templatePath('_gitignore'),
+			this.destinationPath('.gitignore'),
+		);
+
+		this.fs.copy(
+			this.templatePath('package.json'),
+			this.destinationPath('package.json'),
+		);
+
+
+		this.fs.copy(this.templatePath('app'), this.destinationPath('app'));
+
+	}
+
+	end(){
+		this.log(' ');
+		this.log('Finished Generating! Happy Coding')
+	}
+
 
 
 };
