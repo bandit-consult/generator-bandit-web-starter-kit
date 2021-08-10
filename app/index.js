3b0e792bf1e7f627726a335e8265aacf6cb5a5cf
Kofi Ramos Amoussou <karsoft92@gmail.com>
2021-08-10 19:08:49 +0000

    init

---
+++ app/index.js
@@ -0,0 +1,12 @@
+const Generator = require('yeoman-generator');
+
+module.exports = class extends Generator{
+
+	// welcome message
+	welcome(){
+		this.log('Welcom to bandit web starter kit !!')
+	}
+
+
+
+};
